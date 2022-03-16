30995956842c80195dece0dddcfe7f3136662125
Nick Tyler <nicholas.s.tyler.4@gmail.com>
2022-03-16 19:57:19 -0400

    Function to move files when completed

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     url="https://github.com/tylern4/pagurus",
     author="Nick Tyler",
     author_email="tylern@lbl.gov",
-    version='0.1.1',
+    version='0.1.2',
     scripts=glob('bin/*'),
     install_requires=[
         'psutil==5.8.0',
