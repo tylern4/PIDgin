c75effa1fbd49c567d19f4af2d277e19e45cddb2
Nick Tyler <nicholas.s.tyler.4@gmail.com>
2022-05-16 15:09:57 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     url="https://github.com/tylern4/pagurus",
     author="Nick Tyler",
     author_email="tylern@lbl.gov",
-    version=version,
+    version='1.0.1',
     scripts=glob('bin/*'),
     install_requires=[
         'psutil==5.8.0',
