b8d06ba05fdce8c2def54e93fb27ad7ec3ce439e
Steve Chan <sychan168@gmail.com>
2022-10-31 17:00:26 -0700

    Added tests for injecting env vars to output

--- bin/test_FileWriter.py
+++ bin/test_FileWriter.py
@@ -1,5 +1,6 @@
 import pytest
 import json
+import os
 from importlib.util import spec_from_loader, module_from_spec
 from importlib.machinery import SourceFileLoader 
 
@@ -49,3 +50,36 @@ def test_FileWriter_json(tmp_path):
         jsonout = json.dumps(dict(zip(header,[0,1,2])))
         assert( lines[0] == jsonout)
     fw.outfile.unlink()
+
+def test_FileWriter_envvar(tmp_path):
+    print("Output file path", tmp_path)
+    header = ['name1',"name2","name3"]
+    outfile = tmp_path/f"test.csv"
+
+    # Clear out environment variables we will be testing with
+    try:
+        os.environ.pop("testytesty")
+        os.environ.pop("testytoasty")
+    except:
+        pass
+    with pytest.raises(KeyError) as e:
+        fw = pagurus.FileWriter(outfile=outfile, header=header,write_header=True,jsonout=True,env=["testytesty","testytoasty"])
+    os.environ['testytesty'] = 'test'
+    os.environ['testytoasty'] = 'test2'
+    fw = pagurus.FileWriter(outfile=outfile, header=header,write_header=True,jsonout=True,env=["testytesty","testytoasty"])
+    header2 = header+["testytesty","testytoasty"]
+
+    assert( fw.header == header2)
+    assert( fw.write_header == False)
+    assert( fw.outfile == outfile)
+    assert( outfile.is_file())
+    fw.write(0,1,2,"test","test2")
+    fw.close()
+    with open(outfile) as f:
+        contents = f.read()
+        print(contents)
+        lines = contents.splitlines()
+        jsonout = json.dumps(dict(zip(header2,[0,1,2,"test","test2"])))
+        assert( lines[0] == jsonout)
+    
+
