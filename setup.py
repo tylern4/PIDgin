66c814a162ad794de71ef10958c86703996bd8ef
Nick Tyler <nicholas.s.tyler.4@gmail.com>
2022-11-04 12:54:01 -0700

    Bump version number and readme

--- setup.py
+++ setup.py
@@ -22,8 +22,9 @@ setup(
     url="https://github.com/tylern4/pagurus",
     author="Nick Tyler",
     author_email="tylern@lbl.gov",
-    version='1.1',
+    version='1.2',
     scripts=glob('bin/*'),
+    py_modules=[],
     install_requires=[
         'psutil==5.8.0',
     ],
